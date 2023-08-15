33261ffb6e689c425863789381e33973db6833ab
Ridho Purnomo <72849578+ridopurnomo86@users.noreply.github.com>
2023-08-15 16:10:47 +0700

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YelpCamp</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Testing</h1>
+</body>
+</html>
